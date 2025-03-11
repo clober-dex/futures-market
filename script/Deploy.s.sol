1aac07935da67a835ce9aa1e9a5fd80fd82dab01
JChoy <jchoyeclipse@gmail.com>
2025-03-11 15:33:34 +0900

    feat: fix deploy script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -17,35 +17,57 @@ contract DeployScript is Script {
 
     function setUp() public {}
 
-    function run() public {
+    function deployOracle() public returns (address oracle) {
         vm.startBroadcast();
 
         Deployer deployer = Deployer.wrap(msg.sender);
         address owner = msg.sender;
         address pyth = 0x2880aB155794e7179c9eE2e38200202908C17B43;
         uint256 interval = 60;
-        PythOracle oracle = PythOracle(
-            address(
-                new ERC1967Proxy(
-                    address(new PythOracle(pyth, interval)),
-                    abi.encodeWithSelector(PythOracle.initialize.selector, owner)
-                )
+        address implementation =
+            deployer.create2(abi.encodePacked(type(PythOracle).creationCode, abi.encode(pyth, interval)));
+        console.log("Oracle implementation deployed at", address(implementation));
+
+        bytes32 salt = bytes32(
+            abi.encodePacked(Deployer.unwrap(deployer), hex"00", bytes11(keccak256(abi.encode("PythOracle", 0))))
+        );
+        oracle = deployer.create3(
+            salt,
+            abi.encodePacked(
+                type(ERC1967Proxy).creationCode,
+                abi.encode(implementation, abi.encodeWithSelector(PythOracle.initialize.selector, owner))
             )
         );
-        console.log("Oracle deployed at", address(oracle));
+        console.log("Oracle deployed at", oracle);
+    }
 
-        address diamond = address(new FuturesMarket(owner));
-        address debtTokenImpl = address(new Debt(diamond));
+    function deployMarket(address oracle) public {
+        vm.startBroadcast();
+
+        Deployer deployer = Deployer.wrap(msg.sender);
+        address owner = msg.sender;
+
+        bytes32 salt = bytes32(
+            abi.encodePacked(Deployer.unwrap(deployer), hex"00", bytes11(keccak256(abi.encode("FuturesMarket", 0))))
+        );
+        address diamond = deployer.create3(salt, abi.encodePacked(type(FuturesMarket).creationCode, abi.encode(owner)));
+        address debtTokenImpl = deployer.create2(abi.encodePacked(type(Debt).creationCode, abi.encode(diamond)));
         console.log("Diamond deployed at", address(diamond));
         console.log("DebtTokenImpl deployed at", address(debtTokenImpl));
 
         IDiamond.FacetCut[] memory cut = new IDiamond.FacetCut[](6);
         cut[0] = deployer.deployFlashLoanFacet();
+        console.log("FlashLoanFacet deployed at", address(cut[0].facetAddress));
         cut[1] = deployer.deployMarketManagerFacet(address(oracle), debtTokenImpl);
+        console.log("MarketManagerFacet deployed at", address(cut[1].facetAddress));
         cut[2] = deployer.deployMarketPositionFacet(address(oracle));
+        console.log("MarketPositionFacet deployed at", address(cut[2].facetAddress));
         cut[3] = deployer.deployMarketViewFacet();
+        console.log("MarketViewFacet deployed at", address(cut[3].facetAddress));
         cut[4] = deployer.deployOwnershipFacet();
+        console.log("OwnershipFacet deployed at", address(cut[4].facetAddress));
         cut[5] = deployer.deployUtilsFacet();
+        console.log("UtilsFacet deployed at", address(cut[5].facetAddress));
 
         address init = address(new Init());
 
