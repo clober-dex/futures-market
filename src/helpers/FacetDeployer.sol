d4c7baa288784299f3f0f91288a4484014ff6640
JChoy <jchoyeclipse@gmail.com>
2025-03-12 16:57:55 +0900

    feat: make upgrade script

--- src/helpers/FacetDeployer.sol
+++ src/helpers/FacetDeployer.sol
@@ -3,7 +3,7 @@
 pragma solidity ^0.8.0;
 
 import {IERC3156FlashLender} from "@openzeppelin/contracts/interfaces/IERC3156FlashLender.sol";
-
+import {IDiamondLoupe} from "../interfaces/IDiamondLoupe.sol";
 import {ICreateX} from "../interfaces/ICreateX.sol";
 import {IDiamond} from "../interfaces/IDiamond.sol";
 import {IMarketManager} from "../interfaces/IMarketManager.sol";
@@ -46,30 +46,15 @@ library FacetDeployer {
         require(computedAddress == deployed, "Address does not match");
     }
 
-    function deployFlashLoanFacet(Deployer deployer) internal returns (IDiamond.FacetCut memory) {
-        address flashLoanFacet = create2(deployer, type(FlashLoanFacet).creationCode);
-
+    function getFlashLoanFacetSelectors() internal pure returns (bytes4[] memory) {
         bytes4[] memory functionSelectors = new bytes4[](3);
         functionSelectors[0] = IERC3156FlashLender.maxFlashLoan.selector;
         functionSelectors[1] = IERC3156FlashLender.flashFee.selector;
         functionSelectors[2] = IERC3156FlashLender.flashLoan.selector;
-
-        return IDiamond.FacetCut({
-            facetAddress: flashLoanFacet,
-            action: IDiamond.FacetCutAction.Add,
-            functionSelectors: functionSelectors
-        });
+        return functionSelectors;
     }
 
-    function deployMarketManagerFacet(Deployer deployer, address oracle, address debtTokenImpl)
-        internal
-        returns (IDiamond.FacetCut memory)
-    {
-        bytes memory initCode =
-            abi.encodePacked(type(MarketManagerFacet).creationCode, abi.encode(oracle, debtTokenImpl));
-
-        address marketManagerFacet = create2(deployer, initCode);
-
+    function getMarketManagerFacetSelectors() internal pure returns (bytes4[] memory) {
         bytes4[] memory functionSelectors = new bytes4[](7);
         functionSelectors[0] = IMarketManager.open.selector;
         functionSelectors[1] = IMarketManager.settle.selector;
@@ -78,19 +63,10 @@ library FacetDeployer {
         functionSelectors[4] = IMarketManager.changeLtv.selector;
         functionSelectors[5] = IMarketManager.changeLiquidationThreshold.selector;
         functionSelectors[6] = IMarketManager.changeMinDebt.selector;
-
-        return IDiamond.FacetCut({
-            facetAddress: marketManagerFacet,
-            action: IDiamond.FacetCutAction.Add,
-            functionSelectors: functionSelectors
-        });
+        return functionSelectors;
     }
 
-    function deployMarketPositionFacet(Deployer deployer, address oracle) internal returns (IDiamond.FacetCut memory) {
-        bytes memory initCode = abi.encodePacked(type(MarketPositionFacet).creationCode, abi.encode(oracle));
-
-        address marketPositionFacet = create2(deployer, initCode);
-
+    function getMarketPositionFacetSelectors() internal pure returns (bytes4[] memory) {
         bytes4[] memory functionSelectors = new bytes4[](7);
         functionSelectors[0] = IMarketPosition.deposit.selector;
         functionSelectors[1] = IMarketPosition.withdraw.selector;
@@ -99,49 +75,28 @@ library FacetDeployer {
         functionSelectors[4] = IMarketPosition.liquidate.selector;
         functionSelectors[5] = IMarketPosition.redeem.selector;
         functionSelectors[6] = IMarketPosition.close.selector;
-
-        return IDiamond.FacetCut({
-            facetAddress: marketPositionFacet,
-            action: IDiamond.FacetCutAction.Add,
-            functionSelectors: functionSelectors
-        });
+        return functionSelectors;
     }
 
-    function deployMarketViewFacet(Deployer deployer) internal returns (IDiamond.FacetCut memory) {
-        address marketViewFacet = create2(deployer, type(MarketViewFacet).creationCode);
-
+    function getMarketViewFacetSelectors() internal pure returns (bytes4[] memory) {
         bytes4[] memory functionSelectors = new bytes4[](3);
         functionSelectors[0] = IMarketView.getMarket.selector;
         functionSelectors[1] = IMarketView.getPosition.selector;
         functionSelectors[2] = IMarketView.isSettled.selector;
-
-        return IDiamond.FacetCut({
-            facetAddress: marketViewFacet,
-            action: IDiamond.FacetCutAction.Add,
-            functionSelectors: functionSelectors
-        });
+        return functionSelectors;
     }
 
-    function deployOwnershipFacet(Deployer deployer) internal returns (IDiamond.FacetCut memory) {
-        address ownershipFacet = create2(deployer, type(OwnershipFacet).creationCode);
-
+    function getOwnershipFacetSelectors() internal pure returns (bytes4[] memory) {
         bytes4[] memory functionSelectors = new bytes4[](5);
         functionSelectors[0] = IOwnership.owner.selector;
         functionSelectors[1] = IOwnership.pendingOwner.selector;
         functionSelectors[2] = IOwnership.renounceOwnership.selector;
         functionSelectors[3] = IOwnership.transferOwnership.selector;
         functionSelectors[4] = IOwnership.acceptOwnership.selector;
-
-        return IDiamond.FacetCut({
-            facetAddress: ownershipFacet,
-            action: IDiamond.FacetCutAction.Add,
-            functionSelectors: functionSelectors
-        });
+        return functionSelectors;
     }
 
-    function deployUtilsFacet(Deployer deployer) internal returns (IDiamond.FacetCut memory) {
-        address utilsFacet = create2(deployer, type(UtilsFacet).creationCode);
-
+    function getUtilsFacetSelectors() internal pure returns (bytes4[] memory) {
         bytes4[] memory functionSelectors = new bytes4[](7);
         functionSelectors[0] = IUtils.permit.selector;
         functionSelectors[1] = IUtils.multicall.selector;
@@ -150,11 +105,96 @@ library FacetDeployer {
         functionSelectors[4] = 0xdbd035ff; // extsload(bytes32[])
         functionSelectors[5] = 0xf135baaa; // exttload(bytes32)
         functionSelectors[6] = 0x9bf6645f; // exttload(bytes32[])
+        return functionSelectors;
+    }
+
+    function getFlashLoanFacetInitCode() internal pure returns (bytes memory) {
+        return type(FlashLoanFacet).creationCode;
+    }
+
+    function getMarketManagerFacetInitCode(address oracle, address debtTokenImpl)
+        internal
+        pure
+        returns (bytes memory)
+    {
+        return abi.encodePacked(type(MarketManagerFacet).creationCode, abi.encode(oracle, debtTokenImpl));
+    }
+
+    function getMarketPositionFacetInitCode(address oracle) internal pure returns (bytes memory) {
+        return abi.encodePacked(type(MarketPositionFacet).creationCode, abi.encode(oracle));
+    }
+
+    function getMarketViewFacetInitCode() internal pure returns (bytes memory) {
+        return type(MarketViewFacet).creationCode;
+    }
+
+    function getOwnershipFacetInitCode() internal pure returns (bytes memory) {
+        return type(OwnershipFacet).creationCode;
+    }
+
+    function getUtilsFacetInitCode() internal pure returns (bytes memory) {
+        return type(UtilsFacet).creationCode;
+    }
+
+    function deployFlashLoanFacet(Deployer deployer) internal returns (IDiamond.FacetCut memory) {
+        address flashLoanFacet = create2(deployer, getFlashLoanFacetInitCode());
+        return IDiamond.FacetCut({
+            facetAddress: flashLoanFacet,
+            action: IDiamond.FacetCutAction.Add,
+            functionSelectors: getFlashLoanFacetSelectors()
+        });
+    }
+
+    function deployMarketManagerFacet(Deployer deployer, address oracle, address debtTokenImpl)
+        internal
+        returns (IDiamond.FacetCut memory)
+    {
+        address marketManagerFacet = create2(deployer, getMarketManagerFacetInitCode(oracle, debtTokenImpl));
+
+        return IDiamond.FacetCut({
+            facetAddress: marketManagerFacet,
+            action: IDiamond.FacetCutAction.Add,
+            functionSelectors: getMarketManagerFacetSelectors()
+        });
+    }
+
+    function deployMarketPositionFacet(Deployer deployer, address oracle) internal returns (IDiamond.FacetCut memory) {
+        address marketPositionFacet = create2(deployer, getMarketPositionFacetInitCode(oracle));
+
+        return IDiamond.FacetCut({
+            facetAddress: marketPositionFacet,
+            action: IDiamond.FacetCutAction.Add,
+            functionSelectors: getMarketPositionFacetSelectors()
+        });
+    }
+
+    function deployMarketViewFacet(Deployer deployer) internal returns (IDiamond.FacetCut memory) {
+        address marketViewFacet = create2(deployer, getMarketViewFacetInitCode());
+
+        return IDiamond.FacetCut({
+            facetAddress: marketViewFacet,
+            action: IDiamond.FacetCutAction.Add,
+            functionSelectors: getMarketViewFacetSelectors()
+        });
+    }
+
+    function deployOwnershipFacet(Deployer deployer) internal returns (IDiamond.FacetCut memory) {
+        address ownershipFacet = create2(deployer, getOwnershipFacetInitCode());
+
+        return IDiamond.FacetCut({
+            facetAddress: ownershipFacet,
+            action: IDiamond.FacetCutAction.Add,
+            functionSelectors: getOwnershipFacetSelectors()
+        });
+    }
+
+    function deployUtilsFacet(Deployer deployer) internal returns (IDiamond.FacetCut memory) {
+        address utilsFacet = create2(deployer, getUtilsFacetInitCode());
 
         return IDiamond.FacetCut({
             facetAddress: utilsFacet,
             action: IDiamond.FacetCutAction.Add,
-            functionSelectors: functionSelectors
+            functionSelectors: getUtilsFacetSelectors()
         });
     }
 }
