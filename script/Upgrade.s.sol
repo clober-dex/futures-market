f57a6ebba0ca415c3faed4724b3fe702f3b36bb0
JChoy <jchoyeclipse@gmail.com>
2025-04-15 15:59:09 +0900

    feat: add updateOracle script

--- script/Upgrade.s.sol
+++ script/Upgrade.s.sol
@@ -7,6 +7,7 @@ import {CreateX} from "diamond/helpers/CreateX.sol";
 import {DiamondScript} from "diamond/helpers/DiamondScript.sol";
 import {Debt} from "../src/Debt.sol";
 import {Constant} from "../src/helpers/Constant.sol";
+import {PythOracle} from "../src/PythOracle.sol";
 
 contract UpgradeScript is DiamondScript("FuturesMarket") {
     using stdJson for string;
@@ -23,5 +24,27 @@ contract UpgradeScript is DiamondScript("FuturesMarket") {
         (string[] memory facetNames, bytes[] memory facetArgs) = Constant.getFacetData(oracle, debtTokenImpl);
 
         upgrade(facetNames, facetArgs, address(0), "");
+    }
+
+    function upgradeOracle() public broadcast {
+        string memory json = loadDeployment();
+        address deployer = msg.sender;
+        address pyth = Constant.PYTH_FEED_ADDRESS;
+
+        address implementation =
+            CreateX.create2(deployer, abi.encodePacked(type(PythOracle).creationCode, abi.encode(pyth)));
+        console.log("New Oracle implementation deployed at", address(implementation));
+
+        address oracle = json.readAddress(".PythOracle");
+        PythOracle(oracle).upgradeToAndCall(implementation, "");
+        console.log("Oracle upgraded to", address(implementation));
+
+        // Update deployment
+        string memory path = getDeploymentPath();
+        if (vm.exists(path)) {
+            vm.serializeJson("root key", vm.readFile(path));
+        }
+        string memory newJson = vm.serializeAddress("root key", "PythOracle_Implementation", implementation);
+        vm.writeJson(newJson, path);
     }
 }
