ad803a4643a8a46f7b7a227c0ca7cf923b5e14a7
graykode <nlkey2022@gmail.com>
2025-07-09 11:49:11 +0900

    feat: add fee management to RouterGateway

--- src/RouterGateway.sol
+++ src/RouterGateway.sol
@@ -12,13 +12,19 @@ import "@openzeppelin/contracts/utils/ReentrancyGuardTransient.sol";
 contract RouterGateway is UUPSUpgradeable, Ownable2Step, Initializable, ReentrancyGuardTransient {
     using SafeERC20 for IERC20;
 
+    uint256 public constant FEE_PRECISION = 100_000; // 1e5
+    uint256 public constant MAX_FEE_BPS = 100; // 0.1%
+
     mapping(address => mapping(bytes4 => bool)) public allowedRouterMethods;
+    address public feeRecipient;
 
     error InvalidAmount();
     error RouterCallFailed(string reason);
     error TransferFailed();
     error MethodNotAllowed();
     error InsufficientAmountOut();
+    error FeeTooHigh();
+    error InvalidFeeRecipient();
 
     event RouterMethodAdded(address indexed router, bytes4 method);
     event RouterMethodRemoved(address indexed router, bytes4 method);
@@ -32,13 +38,28 @@ contract RouterGateway is UUPSUpgradeable, Ownable2Step, Initializable, Reentran
         bytes4 method
     );
 
+    event FeeCollected(address indexed recipient, address indexed token, uint256 amount);
+
     constructor() Ownable(msg.sender) {}
 
-    function initialize(address initialOwner) external initializer {
+    function initialize(address initialOwner, address initialFeeRecipient) external initializer {
         _transferOwnership(initialOwner);
+        setFeeRecipient(initialFeeRecipient);
+    }
+
+    function setFeeRecipient(address newRecipient) public onlyOwner {
+        if (newRecipient == address(0)) revert InvalidFeeRecipient();
+        feeRecipient = newRecipient;
     }
 
     function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
+
+    function _calculateActualFee(uint256 amountOut, uint256 fee) internal pure returns (uint256) {
+        uint256 maxFee = (amountOut * MAX_FEE_BPS) / FEE_PRECISION;
+        uint256 actualFee = fee == 0 ? maxFee : fee;
+        if (actualFee > maxFee) actualFee = maxFee;
+        return actualFee;
+    }
 
     function addRouterMethod(address router, bytes4 method) external onlyOwner {
         allowedRouterMethods[router][method] = true;
@@ -59,12 +80,15 @@ contract RouterGateway is UUPSUpgradeable, Ownable2Step, Initializable, Reentran
         }
     }
 
-    function swap(address inToken, address outToken, uint256 amountIn, uint256 minAmountOut, address router, bytes calldata data)
-        external
-        payable
-        nonReentrant
-        returns (uint256 amountOut)
-    {
+    function swap(
+        address inToken,
+        address outToken,
+        uint256 amountIn,
+        uint256 minAmountOut,
+        address router,
+        bytes calldata data,
+        uint256 fee
+    ) external payable nonReentrant returns (uint256 amountOut) {
         bytes4 method = bytes4(data[0:4]);
         if (!allowedRouterMethods[router][method]) revert MethodNotAllowed();
 
@@ -88,18 +112,34 @@ contract RouterGateway is UUPSUpgradeable, Ownable2Step, Initializable, Reentran
             IERC20(inToken).forceApprove(router, 0);
         }
 
+        amountOut = outToken == address(0)
+            ? address(this).balance - amountOut
+            : IERC20(outToken).balanceOf(address(this)) - amountOut;
+        if (amountOut < minAmountOut) revert InsufficientAmountOut();
+
+        uint256 actualFee = _calculateActualFee(amountOut, fee);
+        uint256 amountToSend = amountOut - actualFee;
+
         if (outToken == address(0)) {
-            amountOut = address(this).balance - amountOut;
-            if (amountOut < minAmountOut) revert InsufficientAmountOut();
-            (success,) = payable(msg.sender).call{value: amountOut}("");
+            (success,) = payable(msg.sender).call{value: amountToSend}("");
             if (!success) revert TransferFailed();
+
+            if (actualFee > 0) {
+                (success,) = payable(feeRecipient).call{value: actualFee}("");
+                if (!success) revert TransferFailed();
+            }
         } else {
-            amountOut = IERC20(outToken).balanceOf(address(this)) - amountOut;
-            if (amountOut < minAmountOut) revert InsufficientAmountOut();
-            IERC20(outToken).safeTransfer(msg.sender, amountOut);
+            IERC20(outToken).safeTransfer(msg.sender, amountToSend);
+            if (actualFee > 0) {
+                IERC20(outToken).safeTransfer(feeRecipient, actualFee);
+            }
         }
 
         emit Swap(msg.sender, inToken, outToken, amountIn, amountOut, router, method);
+
+        if (actualFee > 0) {
+            emit FeeCollected(feeRecipient, outToken, actualFee);
+        }
     }
 
     receive() external payable {}
